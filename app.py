a496b7137f9c5a0ae95910ae88fc5fdb13bac7fa
Nitsofts Inc <100211678+nitsofts@users.noreply.github.com>
2024-02-15 00:44:54 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import logging
 from base64 import b64encode
 import requests
-from flask import Flask
+from flask import Flask, jsonify  # Make sure jsonify is imported here
 import os
 import json
 import time

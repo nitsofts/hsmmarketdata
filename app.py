5e14fc11e54b2fe096f6a71a3d5339153a2ea922
Nitsofts Inc <100211678+nitsofts@users.noreply.github.com>
2024-02-21 20:11:24 +1100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request
 import os
 import json
 from bs4 import BeautifulSoup
-import time 
+import time
 
 # Basic logging setup for debugging
 logging.basicConfig(level=logging.INFO)
@@ -16,6 +16,48 @@ app = Flask(__name__)
 GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')  # GitHub API token
 REPO_NAME = 'nitsofts/hsmmarketdata'  # Repository name on GitHub
 BRANCH = 'main'  # Branch to update in the repository
+
+# This function is for writing updated data on github page
+# GitHub Update Functions
+def update_data_on_github(file_path, data, data_type):
+    url = f'https://api.github.com/repos/{REPO_NAME}/contents/{file_path}'
+    headers = {
+        'Authorization': f'token {GITHUB_TOKEN}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    response = requests.get(url, headers=headers)
+    sha = response.json().get('sha') if response.status_code == 200 else None
+    content = b64encode(json.dumps(data, ensure_ascii=False).encode('utf-8')).decode('utf-8')
+    update_data = {
+        'message': f'Update {file_path}',
+        'content': content,
+        'branch': BRANCH,
+    }
+    if sha:
+        update_data['sha'] = sha
+    put_response = requests.put(url, headers=headers, json=update_data)
+    if put_response.status_code in [200, 201]:
+        logging.info(f"Successfully updated {file_path} in repository.")
+        update_data_refresh_timestamp(data_type)
+        return True, f'{data_type} updated successfully'
+    else:
+        logging.error(f"Failed to update {file_path}. Response: {put_response.text}")
+        return False, put_response.text
+
+# Function to update timestamp in dataRefresh.json
+def update_data_refresh_timestamp(data_type):
+    data_refresh_file_path = 'dataRefresh.json'
+    try:
+        current_timestamp = int(time.time() * 1000)
+        with open(data_refresh_file_path, 'r') as file:
+            data_refresh = json.load(file)
+            data_refresh[data_type] = current_timestamp
+        with open(data_refresh_file_path, 'w') as file:
+            json.dump(data_refresh, file, indent=2)
+        logging.info(f"Successfully updated timestamp for {data_type} in dataRefresh.json.")
+    except Exception as e:
+        logging.error(f"Failed to update timestamp for {data_type} in dataRefresh.json. Error: {e}")
+
 
 # Function for fetching and writing all TOP PERFORMERS data into github page
 def fetch_and_update_top_performers(limit):
@@ -40,15 +82,15 @@ def fetch_and_update_top_performers(limit):
     for indicator in indicators:
         data = fetch_market_movers(indicator)
         file_path = f'response/top{indicator.capitalize()}.json'
-        success, message = update_data_on_github(file_path, data)
+        success, message = update_data_on_github(file_path, data, indicator)
         all_data[indicator] = {'success': success, 'message': message}
 
     return all_data
-    
+
 # Function for scraping PROSPECTUS
 def scrape_prospectus(page_numbers):
     combined_data = []
-    
+
     def get_prospectus_size(url):
         try:
             response = requests.head(url)
@@ -57,7 +99,7 @@ def scrape_prospectus(page_numbers):
             return file_size_mb
         except Exception:
             return "N/A"  # Return "N/A" if size can't be calculated
-    
+
     for page_number in page_numbers:
         url = f"https://www.sebon.gov.np/prospectus?page={page_number}"
         response = requests.get(url)
@@ -119,47 +161,17 @@ def scrape_cdsc_data():
 
     # Sort the data so that items with "imp": "true" are at the top
     data.sort(key=lambda x: x['imp'], reverse=True)
-    
+
     return data
 
 
-# This function is for writing updated data on github page
-# GitHub Update Functions
-def update_data_on_github(file_path, data):
-    url = f'https://api.github.com/repos/{REPO_NAME}/contents/{file_path}'
-    headers = {
-        'Authorization': f'token {GITHUB_TOKEN}',
-        'Accept': 'application/vnd.github.v3+json'
-    }
-    response = requests.get(url, headers=headers)
-    sha = response.json().get('sha') if response.status_code == 200 else None
-    content = b64encode(json.dumps(data, ensure_ascii=False).encode('utf-8')).decode('utf-8')
-    update_data = {
-        'message': f'Update {file_path}',
-        'content': content,
-        'branch': BRANCH,
-    }
-    if sha:
-        update_data['sha'] = sha
-    put_response = requests.put(url, headers=headers, json=update_data)
-    if put_response.status_code in [200, 201]:
-        logging.info(f"Successfully updated {file_path} in repository.")
-        return True, f'{file_path} updated successfully'
-    else:
-        logging.error(f"Failed to update {file_path}. Response: {put_response.text}")
-        return False, put_response.text
-
-
-
-
 # API Endpoints to make requests -
-
 @app.route('/get_top_performers', methods=['GET'])
 def get_top_performers():
     limit = request.args.get('limit', default=100, type=int)
     data = fetch_and_update_top_performers(limit)
     return jsonify(data)
-    
+
 # Prospectus: /get_prospectus for all 5 pages (1,2,3,4,5)
 # Prospectus: /get_prospectus?pages=1,2 for specific set of pages
 @app.route('/get_prospectus', methods=['GET'])
@@ -168,19 +180,18 @@ def get_prospectus():
     pages = [int(page) for page in pages_str.split(',')]
     data = scrape_prospectus(pages)
     file_path = 'response/prospectus.json'
-    success, message = update_data_on_github(file_path, data)
+    success, message = update_data_on_github(file_path, data, 'prospectus')
     if success:
         return jsonify(data)
     else:
         return jsonify({'success': False, 'message': f'Failed to update prospectus data on GitHub. Error: {message}'})
-
 
 # CDSC DATA: /get_cdsc_data all cdsc data
 @app.route('/get_cdsc_data', methods=['GET'])
 def get_cdsc_data():
     data = scrape_cdsc_data()  # You need to define this function
     file_path = 'response/cdscdata.json'
-    success, message = update_data_on_github(file_path, data)
+    success, message = update_data_on_github(file_path, data, 'cdscData')
     if success:
         response = {'success': True, 'message': 'CDSC data updated on GitHub.'}
     else:

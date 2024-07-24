59f80dcf1e2d0fc4a875c2707f35e924c2880f17
Nitsofts Inc. <100211678+nitsofts@users.noreply.github.com>
2024-07-24 21:07:56 +1000

    Update top_performers.py

--- routes/top_performers.py
+++ routes/top_performers.py
@@ -10,7 +10,7 @@ def get_top_performers():
     limit = request.args.get('limit', default=100, type=int)
     indicator = request.args.get('indicator', default='gainers', type=str)
     
-    if indicator not in ['turnover', 'gainers', 'losers', 'sharestraded', 'transactions']:
+    if indicator not in ['turnover', 'gainers', 'losers', 'sharestraded', 'transactions', 'all']:
         return jsonify({'success': False, 'message': 'Invalid indicator specified'}), 400
     
     try:
@@ -21,17 +21,25 @@ def get_top_performers():
         return jsonify({'success': False, 'message': 'Failed to retrieve top performers data.'}), 500
 
 def fetch_top_performers(limit, specific_indicator):
-    result_data = {}
+    result_data = []
     current_timestamp = int(time.time() * 1000)
-    url = f"https://nepalipaisa.com/api/GetTopMarketMovers?indicator={specific_indicator}&sectorCode=&limit={limit}&_={current_timestamp}"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.json()
-        result_data = data.get('result', [])  # Extract only the 'result' part
-        for item in result_data:
-            item['type'] = specific_indicator
+    
+    if specific_indicator == 'all':
+        indicators = ['turnover', 'gainers', 'losers', 'sharestraded']
     else:
-        logging.error(f"Error fetching {specific_indicator}: {response.status_code}")
+        indicators = [specific_indicator]
+    
+    for indicator in indicators:
+        url = f"https://nepalipaisa.com/api/GetTopMarketMovers?indicator={indicator}&sectorCode=&limit={limit}&_={current_timestamp}"
+        response = requests.get(url)
 
+        if response.status_code == 200:
+            data = response.json()
+            fetched_data = data.get('result', [])  # Extract only the 'result' part
+            for item in fetched_data:
+                item['type'] = indicator
+            result_data.extend(fetched_data)  # Combine data from all indicators
+        else:
+            logging.error(f"Error fetching {indicator}: {response.status_code}")
+    
     return result_data

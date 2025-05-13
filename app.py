7053da50f3edd24ef6ac86a3ec1db09c9ba68b1d
Nitsofts Inc. <100211678+nitsofts@users.noreply.github.com>
2025-05-13 18:35:30 +1000

    Update app.py
    Added auto_post_bp for Automatic Post Feature on Hamro Sharemarket Facebook Page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import logging
 
 from config import API_KEY
 from routes.home import home_bp
+from routes.auto_post import auto_post_bp
 from routes.top_performers import top_performers_bp
 from routes.prospectus import prospectus_bp
 from routes.cdsc_data import cdsc_data_bp
@@ -22,6 +23,7 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})  # Or set a specific origin l
 
 # Register all blueprints
 app.register_blueprint(home_bp)
+app.register_blueprint(auto_post_bp, url_prefix='/api')
 app.register_blueprint(top_performers_bp)
 app.register_blueprint(prospectus_bp)
 app.register_blueprint(cdsc_data_bp)
